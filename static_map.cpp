b89680ecf915453d6dccd4b45b09fb2b8429518b
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-09-28 00:29:21 +0900

    Update static_map.cpp

--- static_map.cpp
+++ static_map.cpp
@@ -1,11 +1,17 @@
+/**
+* キーの追加がない map
+* std::map よりも検索が高速でメモリ効率も良い
+*/
 #include <vector>
 #include <utility>
 #include <algorithm>
 #include <cassert>
-template <typename KEY, typename VAL>
+
+template <class KEY, class VAL>
 class static_map{
 private:
   std::vector<std::pair<KEY,VAL > > a;
+  bool is_built=true;
   int my_lower_bound(const KEY& key)const{
     int ok=a.size(), ng=-1;
     while(ok-ng>1){
@@ -15,19 +21,24 @@ private:
     }
     return ok;
   }
+  void make(void){ std::sort(a.begin(), a.end()); }
 public:
   static_map(void){}
-  void insert(const KEY& key, const VAL& val){
+  void emplace(const KEY& key, const VAL& val){
+    is_built=false;
     a.emplace_back(std::make_pair(key, val));
   }
-  void make(void){ std::sort(a.begin(), a.end()); }
-  bool contains(const KEY& key)const{
+  bool count(const KEY& key)const{
+    if(!is_built){make(); is_built=true;}
     int idx=my_lower_bound(key);
-    return (idx!=a.size())?(a[idx].first==key):false;
+    return idx!=a.size()&&a[idx].first==key;
   }
-  const VAL& operator[](const KEY& key)const{
+  VAL& operator[](const KEY& key){
+    if(!is_built){make(); is_built=true;}
     int idx=my_lower_bound(key);
-    assert((idx!=a.size())?(a[idx].first==key):false);
+    assert(idx!=a.size()&&a[idx].first==key);
     return a[idx].second;
   }
+  int size(void)const{ return a.size(); }
+  const std::vector<std::pair<KEY,VAL > >& get(void){ return a; }
 };
