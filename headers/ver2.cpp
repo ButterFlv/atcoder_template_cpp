ced8aa2f1ee05e8578a5ca46893384515e4b3447
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-10-05 14:45:29 +0900

    Update ver2.cpp

--- headers/ver2.cpp
+++ headers/ver2.cpp
@@ -14,9 +14,10 @@ using pii = pair<int,int>;
 using mint = atcoder::modint998244353;
 
 // Macro
-#define rep(i,n) for(int i=0;i<(int)(n);i++)
-#define gor(i,s,n) for(int i=(s);i<(int)(n);i++)
-#define FOR(i,s,n) for(int i=(s);i<(int)(n);i++)
+#define OVERLOAD_REP(_1, _2, _3, name, ...) name
+#define REP1(i, n) for (auto i = std::decay_t<decltype(n)>{}; (i) != (n); ++(i))
+#define REP2(i, l, r) for (auto i = (l); (i) != (r); ++(i))
+#define rep(...) OVERLOAD_REP(__VA_ARGS__, REP2, REP1)(__VA_ARGS__)
 
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
