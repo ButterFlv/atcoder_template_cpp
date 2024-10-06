6801a9cfefb51fcd8471a9ccb7f90f660173f230
ButterFlv <122111975+ButterFlv@users.noreply.github.com>
2024-10-06 11:23:11 +0900

    Update zaatsu.cpp

--- zaatsu.cpp
+++ zaatsu.cpp
@@ -1,5 +1,6 @@
-#include<algorithm>
-#include<vector>
+#include <algorithm>
+#include <vector>
+#include <cassert>
 template <class T>
 class zaatsu{
 private:
@@ -7,15 +8,21 @@ private:
   bool is_built=true;
 public:
   void insert(const T& x){a.emplace_back(x); is_built=false;}
-  int operator[](const T& x){
+  void make(void){
     if(!is_built){
       std::sort(a.begin(),a.end());
       a.erase(std::unique(a.begin(),a.end()),a.end());
       is_built=true;
     }
+  }
+  int operator[](const T& x){
+    make();
     int idx=std::lower_bound(a.begin(),a.end(),x)-a.begin();
     assert(idx!=a.size()&&a[idx]==x);
     return idx;
   }
-  const T& inv(int k){return a[k];}
+  const T& inv(int k){make(); return a[k];}
+  size_t size(void){return a.size();}
+  auto begin(void){make(); return a.begin();}
+  auto end(void){make(); return a.end();}
 };
